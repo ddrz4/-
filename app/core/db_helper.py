3b6b4b60b7e52b8dc010d018a0e9c0821863eab8
psyhhho~ <samzelcer@gmail.com>
2024-10-18 13:32:35 +0200

    Change db_helper file

--- app/core/db_helper.py
+++ app/core/db_helper.py
@@ -1,47 +1,37 @@
-from typing import AsyncGenerator
-
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
-    AsyncEngine,
     async_sessionmaker,
-    AsyncSession,
 )
 
-from config import settings
+from .config import settings
 
 
-class DatabaseHelper:
-    def __init__(
+class DBHelper:
+    def engine(
         self,
         url: str,
         echo: bool = False,
         echo_pool: bool = False,
         pool_size: int = 5,
         max_overflow: int = 10,
-    ) -> None:
-        self.engine: AsyncEngine = create_async_engine(
+    ):
+        self.engine = create_async_engine(
             url=url,
             echo=echo,
             echo_pool=echo_pool,
             pool_size=pool_size,
             max_overflow=max_overflow,
         )
-        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+
+        self.session = async_sessionmaker(
             bind=self.engine,
             autoflush=False,
             autocommit=False,
             expire_on_commit=False,
         )
 
-    async def dispose(self) -> None:
-        await self.engine.dispose()
 
-    async def session_getter(self):
-        async with self.session_factory() as session:
-            yield session
-
-
-db_helper = DatabaseHelper(
+db_helper = DBHelper(
     url=str(settings.db.db_url_asyncpg),
     echo=settings.db.echo,
     echo_pool=settings.db.echo_pool,
