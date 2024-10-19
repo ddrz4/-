8b44c4a205582a2e5d54571da3a4a1c4dfa3b7a7
psyhhho~ <samzelcer@gmail.com>
2024-10-19 17:47:36 +0200

    move fastapi users routers to apiv1 package

--- app/api/api_v1/fastapi_users_routers.py
+++ app/api/api_v1/fastapi_users_routers.py
@@ -2,7 +2,7 @@ from fastapi_users import FastAPIUsers
 from app.core.types.user_id import UserIdType
 from app.core.models.user import User
 from app.api.dependencies.authentication.backend import authentication_backend
-from .user_manager import get_user_manager
+from ..dependencies.authentication.user_manager import get_user_manager
 
 fastapi_users = FastAPIUsers[User, UserIdType](
     get_user_manager,
