f80af5b5711fc91d8e3a296472f10f38110daa05
psyhhho~ <samzelcer@gmail.com>
2024-10-19 16:33:20 +0200

    Create user manager

---
+++ app/core/authentication/user_manager.py
@@ -0,0 +1,47 @@
+import logging
+from typing import (
+    Optional,
+    TYPE_CHECKING,
+)
+from app.core.config import settings
+from fastapi_users import (
+    BaseUserManager,
+    IntegerIDMixin
+)
+from app.core.models.user import User
+from app.core.types.user_id import UserIdType
+if TYPE_CHECKING:
+    from fastapi import Request
+
+log = logging.getLogger(__name__)
+
+
+class UserManager(IntegerIDMixin, BaseUserManager[User, UserIdType]):
+    reset_password_token_secret = settings.access_token.RESET_PASSWORD_TOKEN_SECRET
+    verification_token_secret = settings.access_token.VEREFICATION_TOKEN_SECRET
+
+    async def on_after_register(
+        self, user: User, request: Optional["Request"] = None,
+    ):
+        log.warning(
+            "User %r has registered.",
+            user.id,
+        )
+
+#    async def on_after_forgot_password(
+#        self, user: User, token: str, request: Optional["Request"] = None,
+#    ):
+#        log.warning(
+#            "User %r has forgot their password. Reset token: %r",
+#            user.id,
+#            token,
+#        )
+#
+#    async def on_after_request_verify(
+#        self, user: User, token: str, request: Optional["Request"] = None,
+#    ):
+#        log.warning(
+#            "Verification requested for user %r. Verification token: %r",
+#            user.id,
+#            token,
+#        )
