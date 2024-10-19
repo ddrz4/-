65dad77706e668f8ddba52d313e7f37880cc389c
psyhhho~ <samzelcer@gmail.com>
2024-10-19 17:14:40 +0200

    move dependencies to inner package

--- app/api/dependencies/authentication/fastapi_users_routers.py
+++ app/api/dependencies/authentication/fastapi_users_routers.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 from app.core.types.user_id import UserIdType
 from app.core.models.user import User
-from app.api.dependencies.backend import authentication_backend
+from app.api.dependencies.authentication.backend import authentication_backend
 from .user_manager import get_user_manager
 
 fastapi_users = FastAPIUsers[User, UserIdType](
