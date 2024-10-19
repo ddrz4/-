79d556565def56e8b46fd9139aac5c53070f2f66
psyhhho~ <samzelcer@gmail.com>
2024-10-19 17:03:19 +0200

    Create users schemas

---
+++ app/core/schemas/user.py
@@ -0,0 +1,14 @@
+from fastapi_users import schemas
+from app.core.types.user_id import UserIdType
+
+
+class UserRead(schemas.BaseUser[UserIdType]):
+    pass
+
+
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
