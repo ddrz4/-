dc95c50a0d38279a19db2a1e57f8393117740e8f
psyhhho~ <samzelcer@gmail.com>
2024-10-19 16:45:44 +0200

    Delete unuseless imports

--- app/api/dependencies/access_tokens.py
+++ app/api/dependencies/access_tokens.py
@@ -3,9 +3,7 @@ from typing import (
     Annotated,
 )
 from fastapi import Depends
-from fastapi_users.db import SQLAlchemyUserDatabase
 from app.core.db_helper import DBHelper
-from app.core.models.user import User
 from app.core.models.access_token import AccessToken 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
