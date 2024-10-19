7cb7f176494ee9d082ce51010a20273bb4753f56
psyhhho~ <samzelcer@gmail.com>
2024-10-19 13:01:17 +0200

    Changes in User model

--- app/core/models/user.py
+++ app/core/models/user.py
@@ -1,5 +1,4 @@
 from typing import TYPE_CHECKING
-
 from fastapi import Depends
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
@@ -8,9 +7,10 @@ from fastapi_users.db import (
 from sqlalchemy.ext.asyncio import AsyncSession
 from .mixins.id_int_pk import IdIntPkMixin
 from .base import Base
+from app.core.types.user_id import UserIdType
+
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
-from app.core.types.user_id import UserIdType
 
 
 
@@ -18,4 +18,4 @@ class User(Base, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
