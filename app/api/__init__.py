f9c785d214b7f93bfefe6ac8eae73ca76c3d18a4
psyhhho~ <samzelcer@gmail.com>
2024-10-23 18:13:59 +0200

    Crate messages router

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,6 +3,7 @@ from fastapi import Depends
 from fastapi.security import HTTPBearer
 from api.auth import router as auth_router
 from api.users import router as users_router
+from api.messages import router as messages_router
 
 http_bearer = HTTPBearer(auto_error=False)
 
@@ -14,5 +15,4 @@ router = APIRouter(
 
 router.include_router(auth_router)
 router.include_router(users_router)
-
-
+router.include_router(messages_router)
